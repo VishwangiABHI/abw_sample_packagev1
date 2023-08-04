80572e6138a6631f75ed41209573640bc1b04609
VishwangiABHI <vishwangi.shah-v@adityabirlacapital.com>
2023-08-04 18:14:46 +0530

    Second commit

--- lib/abw_sample_packagev1.dart
+++ lib/abw_sample_packagev1.dart
@@ -1,7 +1,12 @@
 library abw_sample_packagev1;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:abw_sample_packagev1/webview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  static MaterialApp showCustomAlertBox({required BuildContext context}) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: WebView());
+  }
 }
